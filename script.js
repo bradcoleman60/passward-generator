ab0e397222083cc19a07c89bfb63041f92ea6358
Brad Coleman <bradcoleman60@gmail.com>
2022-12-19 21:59:55 -0800

    Adds some initial thinking about random numbers

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+
+
+//Initial Discovery of creating random numbers
+var randNum = Math.random();
+
+// This creates a random integer
+console.log (randNum)
+
+//This creates a random whole number less than 1000
+console.log (Math.trunc(randNum * 1000))
+
+//This creates a random number between zero and 999 and does it 101 separate times
+// for (var x = 0; x <= 100; x++) {
+//   console.log (x +" - "+(Math.trunc(Math.random() * 1000)))
+// }
+
+//This attempts to limit the random number to a max limit of 100
+for (var x = 0; x <= 100; x++) {
+  if ((Math.trunc(Math.random() * 1000)) < 100)
+  {
+  console.log (x +" - "+(Math.trunc(Math.random() * 1000)))
+  
+  } 
+  else {console.log("no can do")}
+}
